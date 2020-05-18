c9827b73d34c136a3328235dfe8a7a26c15a41c7
Saurabh kacholiya <kacholiyasaurabh.com>
2020-05-18 16:26:00 +0530

    fix

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -18,8 +18,8 @@ module.exports = require('./webpack.base.babel')({
 
   // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
   output: {
-    filename: '[name].[chunkhash].js',
-    chunkFilename: '[name].[chunkhash].chunk.js',
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js',
   },
 
   optimization: {
